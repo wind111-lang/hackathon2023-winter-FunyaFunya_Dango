f08a074ddd0291cdf1b0dacd1c3f90d0ceb462b8
009kin <2011140009m@ed.fuk.kindai.ac.jp>
2023-02-17 12:01:33 +0900

    追加:Homeにスタイルを追加

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import "./Home.css"
 
 const Home = () => {
   const navigation = useNavigate();
   
   return (
-    <div>
+    <div className='regionWrapper'>
         <button className="region" type="button" onClick={() => navigation('/area/1')}>北海道</button>
         <button className="region" type="button" onClick={() => navigation('/area/2')}>東北</button>
         <button className="region" type="button" onClick={() => navigation('/area/3')}>関東</button>
         <button className="region" type="button" onClick={() => navigation('/area/4')}>中部</button>
         <button className="region" type="button" onClick={() => navigation('/area/5')}>近畿</button>
-        <button className="region" type="button"onClick={() => navigation('/area/6')}>中国</button>
-        <button className="region" type="button"onClick={() => navigation('/area/7')}>四国</button>
-        <button className="region" type="button"onClick={() => navigation('/area/8')}>九州</button>
+        <button className="region" type="button" onClick={() => navigation('/area/6')}>中国</button>
+        <button className="region" type="button" onClick={() => navigation('/area/7')}>四国</button>
+        <button className="region" type="button" onClick={() => navigation('/area/8')}>九州</button>
     </div>
   )
 }
