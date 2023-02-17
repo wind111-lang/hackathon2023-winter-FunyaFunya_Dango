e03965f17d1971a4d49d23ad8952b30d3c825e50
nachan28 <torajiro282828@gmail.com>
2023-02-17 15:46:30 +0900

    追加: お気に入り追加機能とお気に入りページ追加

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { fa-solid fa-heart } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
 
 const Card = ({ data }) => {
+  //   useEffect(() => {
+  //     localStorage.setItem("favs", JSON.stringify(localStorage.getItem("favs")));
+  //   }, [])
+  const addFavs = (data) => {
+    if (localStorage.getItem("favs") == null) {
+      localStorage.setItem("favs", JSON.stringify([]));
+    }
+    const favs = JSON.parse(localStorage.getItem("favs"));
+    console.log(favs);
+    favs.push(data);
+    localStorage.setItem("favs", JSON.stringify(favs));
+  };
   return (
     <div className="cardContainer" key={data.title}>
       <div className="postHeader">
@@ -10,13 +23,14 @@ const Card = ({ data }) => {
           <a href={data.url}>{data.title}</a>
         </h3>
         <div className="like">
-          <FontAwesomeIcon icon="fa-solid fa-heart" />
+          {/* <FontAwesomeIcon icon="fa-solid fa-heart" /> */}
           <span>{data.like}</span>
         </div>
       </div>
       <div className="postFooter">
         <p className="companyName">{data.company_name}</p>
         <p className="date">{data.created_at}</p>
+        <button onClick={() => addFavs(data)}>★</button>
       </div>
     </div>
   );
